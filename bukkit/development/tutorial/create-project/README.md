baccb10ad8f0f45872b341c901973aead30cdb24
HimaJyun <HimaJyun@users.noreply.github.com>
2019-06-20 03:48:02 +0900

    add /bukkit/development/tutorial/create-project/

---
+++ bukkit/development/tutorial/create-project/README.md
@@ -0,0 +1,4 @@
+# Bukkitプロジェクトの作成
+
+- [Bukkitプロジェクトの作成 Eclipse編](https://e-craft.io/bukkit/development/tutorial/create-project-eclipse/)
+- [Bukkitプロジェクトの作成 IntelliJ IDEA編](https://e-craft.io/bukkit/development/tutorial/create-project-idea/)
---
+++ bukkit/development/tutorial/create-project/src/main/java/io/e_craft/projectsample/ProjectSample.java
@@ -0,0 +1,10 @@
+package io.e_craft.projectsample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ProjectSample extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, world!");
+    }
+}
